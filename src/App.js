8104b3780e31a4eff6f7a98a116be06c63e88acb
FransKarlsson <70134935+FransKarlsson@users.noreply.github.com>
2022-05-15 12:24:01 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './style.scss'
+import {useState, useEffect} from 'react'
+import icon from './img/icon.png'
 
 function App() {
+  
+  
+  const [products, setproducts] = useState([])
+
+  useEffect(() => {
+    fetch("https://localhost:7110/Product")
+    .then(response => response.json())
+    .then(data => setproducts(data));
+  },[])
+  // console.log(products);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <main>
+        <div id="navbar">
+            <div className="navbar-title">
+                <div id="icon"><img src={icon}/></div>
+                <h1 className="title">Gmarketplace™</h1>  
+            </div>
+  
+            <div className="navbar-buttons">
+                <button className="search">Search</button>
+                <button className="cart">Cart</button>
+            </div>
+
+        </div>
+
+        <div className="slider-container" id="featured">
+            <div className="category-title">
+              <h2>Featured products</h2>
+            </div>
+            <div className="product-items">
+              {
+                products.map(product => 
+                <div className="product">
+                  <div className='product-title'>{product.title}</div>
+                  <div className='image'></div>
+                  <div className='rating'></div>
+                </div>
+                )
+              }
+
+            </div>
+        </div>
+
+
+    </main>
+      
     </div>
-  );
+  )
+  
 }
 
 export default App;
